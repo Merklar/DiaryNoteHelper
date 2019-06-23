f3f1aef5ca04d9b25f90284d93da80ebec12328d
Merklar <uicraor@mail.ru>
2019-06-23 12:15:51 +0300

    Add import image

--- DiaryNoteHelper/MainWindow.xaml.cs
+++ DiaryNoteHelper/MainWindow.xaml.cs
@@ -19,10 +19,50 @@ namespace DiaryNoteHelper
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {   
+        private string imgFileAdress;
+
+        private List<Label> Labels = new List<Label>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ImgDrop_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                foreach (string file in files)
+                {
+                    imgFileAdress = file.ToString();
+                }
+            }
+            
+            try
+            {
+                Uri uri             = new Uri(imgFileAdress);
+                BitmapImage bitmap  = new BitmapImage(uri);
+                ImgContainer.Source = bitmap;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void CreateTextLabel()
+        {
+            Label label = new Label();
+            label.Content = "Label " + Labels.Count + 1;
+            Labels.Add(label);
+        }
+
+        private void CreateLblBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CreateTextLabel();
         }
     }
 }
